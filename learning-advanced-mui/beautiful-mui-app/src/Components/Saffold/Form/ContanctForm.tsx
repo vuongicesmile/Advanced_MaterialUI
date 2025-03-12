ea45c699cca8c354834adf5508a34663d193b4b0
Nguyen Quoc Vuong <vuong.quoc@frogasia.com>
2025-03-12 09:08:00 +0700

    input field - dropdown input - select input - datepicker input

--- learning-advanced-mui/beautiful-mui-app/src/Components/Saffold/Form/ContanctForm.tsx
+++ learning-advanced-mui/beautiful-mui-app/src/Components/Saffold/Form/ContanctForm.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Autocomplete, FormControl, FormGroup, Paper, Select, TextField } from "@mui/material";
+import { Autocomplete, FormControl, FormGroup, ListItemText, Menu, MenuItem, Paper, Select, TextField } from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
 
 const roles = ["React", "Angular", "Python", "NodeJS", "Machine Learning"];
 const skills = ["Software Dev", "Architect", "Designer", "Business Analyst"];
@@ -11,35 +15,65 @@ export default function ContactForm() {
       <form>
         <FormControl>
           <FormGroup
-          row
+            row
           >
-            <TextField 
+            {/*  ô input nè */}
+            <TextField
               id="name"
               name="name"
               label="Nameeee"
               variant='outlined' // tạo ra đường viên xung quanh thành phần, thay vì mặc định phẳng
-             />
-             <Autocomplete  // tạo ra danh sách các tùy chọn dựa trên mảng roles. người dùng có thể nhập vào ô và lựa chọn một trong các vai trò có sẵn
-                            // trường nhập liệu này được hiển thị dưới dạng textField
-             options={roles} // mảng
-             renderInput={(params) => { // hiển thị các phần tử đầu vào (input field) của autocomplete
-              // params chứa các thuộc tính cần thiết để tạo ra trường nhập liệu textField cho autoComplete
-              return (
-                <TextField
-                name="role"
-                {...params}
-                />
-              )
-             }}
-             getOptionLabel={(roleOption) => `${roleOption}`}
-             renderOption={(props,option) => {
-              return (
-                <li {...props}>
-                  {`${option}`}
-                </li>
-              )
-             }}
-             />
+            />
+            {/* ------------------------------------ */}
+            {/*  dropdown input cho phép xóa */}
+            <Autocomplete  // tạo ra danh sách các tùy chọn dựa trên mảng roles. người dùng có thể nhập vào ô và lựa chọn một trong các vai trò có sẵn
+              // trường nhập liệu này được hiển thị dưới dạng textField
+              options={roles} // mảng
+              renderInput={(params) => { // hiển thị các phần tử đầu vào (input field) của autocomplete
+                // params chứa các thuộc tính cần thiết để tạo ra trường nhập liệu textField cho autoComplete
+                return (
+                  <TextField
+                    name="role"
+                    {...params}
+                  />
+                )
+              }}
+              getOptionLabel={(roleOption) => `${roleOption}`}
+              renderOption={(props, option) => {
+                return (
+                  <li {...props}>
+                    {`${option}`}
+                  </li>
+                )
+              }}
+            />
+          </FormGroup>
+           {/* ------------------------------------ */}
+           {/* dropdown chỉ cho phép chọn và chọn lại */}
+          <FormGroup
+            row
+          >
+            <Select>
+              {skills.map((skillName) => {
+                return (
+                  <MenuItem value={skillName} key={skillName}>
+                    <ListItemText primary={skillName} />
+                  </MenuItem>
+                )
+              })}
+            </Select>
+            {/* datepicker input */}
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DesktopDatePicker
+                label="Date"
+                inputFormat="MM/DD/YYYY"
+                renderInput={(params) => {
+                  return <TextField {...params} />
+                }}
+                // value={"abcv"}
+                // onChange={() => { }}
+              />
+            </LocalizationProvider>
           </FormGroup>
         </FormControl>
       </form>
